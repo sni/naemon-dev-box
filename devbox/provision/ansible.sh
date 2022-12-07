e4c23eef9ec6c7ecf396c5517d5e9466d0906ed4
Sven Nierlein <sven@nierlein.de>
2022-12-07 12:57:21 +0100

    fix provisioning

--- devbox/provision/ansible.sh
+++ devbox/provision/ansible.sh
@@ -3,5 +3,8 @@
 mkdir -p /etc/ansible
 echo -e "[defaults]\ngather_timeout = 30" > /etc/ansible/ansible.cfg
 
+export PYTHONPATH=/omd/versions/default/lib/python
+export PATH=/omd/versions/default/bin:$PATH
+
 ansible-playbook -i localhost, "/box/devbox/provision/main.yml" -c local
 exit $?
