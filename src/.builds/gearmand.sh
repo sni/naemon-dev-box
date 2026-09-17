23194f2b8a1a1ce2a047ed10cf833909c3887192
Sven Nierlein <Sven.Nierlein@consol.de>
2026-09-17 21:35:44 +0200

    add gearmand builds from source

---
+++ src/.builds/gearmand.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/bash
+
+set -eu
+
+export CFLAGS="-g -O0 -fsanitize=address -fno-omit-frame-pointer -Wno-error"
+export CXXFLAGS="$CFLAGS"
+export LDFLAGS="-fsanitize=address -fno-omit-frame-pointer"
+
+unset PYTHONPATH
+unset LD_LIBRARY_PATH
+
+autoreconf -s -i
+./configure \
+	--enable-jobserver=no \
+	--disable-libmemcached \
+	--disable-hiredis \
+	--disable-libdrizzle \
+	--disable-libpq \
+	--disable-libtokyocabinet \
+	--without-mysql \
+	--prefix=/
+
+# AX_HARDEN_COMPILER_FLAGS appends a trailing -Werror to CFLAGS/CXXFLAGS after
+# our -Wno-error (last flag wins for GCC). Neutralize it in the generated
+# Makefiles so -Wno-error takes effect.
+find . -name Makefile -exec sed -i 's/-Werror/-Wno-error/g' {} +
+
+make -j 1
+
+mkdir -p dest
+make DESTDIR=$(pwd)/dest install
--- src/.builds/mod_gearman.sh
+++ src/.builds/mod_gearman.sh
@@ -5,6 +5,14 @@ set -eu
 export PKG_CONFIG_PATH="$(pwd)/../naemon-core"
 export C_INCLUDE_PATH="$(pwd)/../naemon-core:$(pwd)/../naemon-core/src"
 
-./autogen.sh --with-gearman=/omd/versions/default --enable-debug --enable-asan
-./configure --with-gearman=/omd/versions/default --enable-debug --enable-asan
+#LIBGEARMAN=/omd/versions/default
+LIBGEARMAN="$(pwd)/../gearmand/dest"
+
+export CFLAGS="-fsanitize=address"
+export LDFLAGS="-fsanitize=address"
+
+OPTS="--with-gearman=$LIBGEARMAN --enable-debug --enable-asan"
+
+./autogen.sh $OPTS
+./configure  $OPTS
 make
